e250ac282acc6a96361baa2e606c2ae3902bcffe
Floofball <59265546+Floofball-uwu@users.noreply.github.com>
2026-01-20 14:55:37 +0100

    Test setup for running Stockfish with game output for analysis

---
+++ Thinker/setup_tests/chessTest.py
@@ -0,0 +1,23 @@
+import chess
+import chess.engine
+import chess.pgn
+
+engine = chess.engine.SimpleEngine.popen_uci(r"C:\_dev\Chessica\Thinker\stockfish-windows-x86-64-avx2\stockfish-windows-x86-64-avx2.exe")
+
+timePerMove = 0.01 #seconds, exact search time
+
+i = 0
+moveBuffer = ""
+
+board = chess.Board()
+while not board.is_game_over():
+    result = engine.play(board, chess.engine.Limit(time=timePerMove), info=chess.engine.INFO_NONE)
+    board.push(result.move)
+
+game = chess.pgn.Game.from_board(board)
+game.headers["Event"] = "Test"
+print(game, file=open("./Thinker/setup_tests/game.pgn", "w"), end="\n\n")
+print("Game complete")
+
+engine.quit()
+
