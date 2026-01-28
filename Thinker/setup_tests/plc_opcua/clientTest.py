d43c25cd38eeb6b3b20612e091d19f3fde1a8355
Floofball <59265546+Floofball-uwu@users.noreply.github.com>
2026-01-28 11:23:58 +0100

    Minimized Client-PLC example

--- Thinker/setup_tests/plc_opcua/clientTest.py
+++ Thinker/setup_tests/plc_opcua/clientTest.py
@@ -1,24 +1,16 @@
 #USE PYTHON 3.13.11
-#Client with PLC as server
+#Client with virtual PLC as server
 
 import asyncio
 from asyncua import Client
 
-url = "opc.tcp://localhost:4840" #"opc.tcp://localhost:4840/chessica"
-namespace = "http://opcua.chessica.io"
-
-#Wrapper around input() to make it run asynchronously
-#Required or else various things break down with the async OPC UA
-async def async_input(prompt: str) -> str:
-    return await asyncio.to_thread(input, prompt)
+url = "opc.tcp://localhost:4840"
 
 async def main():
     print(f"Connecting to {url} ...")
     async with Client(url=url) as client:
-        nsidx = 0 #await client.get_namespace_index(namespace)
-        print(f"Namespace Index for '{namespace}':{nsidx}")
-
-        testInt = client.get_node("ns=5;b=AQAAAKbhKnGK9zM6uvotdobvYEeM925gjPA0VYrgNXmc7yFghvEfJek=")
+        nodeId = "ns=5;b=AQAAAKbhKnGK9zM6uvotdobvYEeM925gjPA0VYrgNXmc7yFghvEfJek="
+        testInt = client.get_node(nodeId)
         print(f"testInt: {await testInt.read_value()}")
         
         while True:
